3c73a7b10a97e0595087eebb1fb5fc0d91eac64d
Jon Marc <jonmarcguz@gmail.com>
2022-10-15 02:51:04 -0700

    Slot_machine_program 2nd build:
    This adds another user input which is the number of lines which the user wants.

--- Slot_machine_program.py
+++ Slot_machine_program.py
@@ -1,6 +1,7 @@
 #global constant done in all capitals
 MAX_LINES = 3
 
+#ALl user input
 #Function to collect deposite through user input
 def deposite():
 #while loop continually ask user for a amount until it is a registered valid amount
@@ -14,15 +15,28 @@ def deposite():
             else:
                 print("Amount must be greater than 0.")
         else:
-            print("please enter a number.")
+            print("Please enter a number.")
     return amount
 
 #collect bet from user
 def get_number_of_lines():
-
+    while True:
+#Added global constant MAX_LINES
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1<= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter valid number of lines.")
+        else:
+            print("please enter a number.")
+    return lines
 
 #where the main program will be in
 def main():
     balance = deposite()
+    lines = get_number_of_lines()
+    print(balance, lines)
 
 main()
