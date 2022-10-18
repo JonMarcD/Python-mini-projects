f13b8e9c9b397d79c0eb81092da4085bf37b81f7
Jon Marc <jonmarcguz@gmail.com>
2022-10-17 18:20:48 -0700

    Gross_pay_program build 3:
    The program now handles non-numeric inputs through try and except, exception handling.
    If a non-numeric is the input the program pops up with an error message.

--- Gross_pay.py
+++ Gross_pay.py
@@ -1,13 +1,18 @@
 #Gross pay program:
+from pickletools import int4
 
-prompt1 = 'How many hours did you work?\n'
-hours = input(prompt1)
-if float(hours) > float(40):
-    rate = 15
-else:  #nested conditional
-    if float(hours) <= float(40):
-        rate =10
 
-#Have to write float any input value can be a decimal which is not an integer
-pay = float(hours) * float(rate)
-print(f"${pay} is your gross pay.")
+print("How many hours did you work?")
+int = input()
+
+try:
+    hours = float(int)
+    if float(hours) > float(40):
+        rate = 15
+    else:  #nested conditional
+        if float(hours) <= float(40):
+            rate =10
+    pay = float(hours) * float(rate)
+    print(f"${pay} is your gross pay.")
+except:
+    print("Error, please enter numeric input")
