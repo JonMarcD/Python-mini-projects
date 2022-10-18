a340ce810c7a3f36165a2f7e3b2d0571b799873d
Jon Marc <jonmarcguz@gmail.com>
2022-10-18 14:48:06 -0700

    Gross_pay_program build 4:
    The program now computes different rates for overtime hours and non-overtime hours.
    This is done by finding the over time hours and multiplying it with the over time rate then adding it with
    non-overtime hours multiplied by normal hourly rate.

--- Gross_pay.py
+++ Gross_pay.py
@@ -5,11 +5,16 @@ int = input()
 try:
     hours = float(int)
     if float(hours) > float(40):
+        ot_hours = float(hours) - float(40)
         rate = 15
+        ot_pay = float(ot_hours) * float(rate)
+        normal_hours = float(40) * float(10)
+        ot_gross = float(ot_pay) + float(normal_hours)
+        print(f"${ot_gross} is your gross pay.")
     else:  #nested conditional
         if float(hours) <= float(40):
             rate =10
-    pay = float(hours) * float(rate)
-    print(f"${pay} is your gross pay.")
+        pay = float(hours) * float(rate)
+        print(f"${pay} is your gross pay.")
 except:
     print("Error, please enter numeric input")
