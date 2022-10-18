a7d21c5b6762ca5931a89ec8075b6f7d05ed30fb
Jon Marc <jonmarcguz@gmail.com>
2022-10-18 16:09:30 -0700

    Gross_pay_program build 5:
    Added a function called computepay(hours, rate). This has the equation for non-overtime pay and takes hours and rate as
    the parameter of the argument.

--- Gross_pay.py
+++ Gross_pay.py
@@ -1,20 +1,26 @@
 #Gross pay program:
+#Has to be at the start because of flow execution
 prompt1 = print("How many hours did you work?")
 int = input()
 
+#function that calculates pay the parameters get are the input to the equation inside the function
+def computepay(hours, rate):
+    pay = float(hours) * float(rate) #hours & rate take whatever is set as the parameter
+    return pay #the return statement is sent to the calling code and used as the functions result
+
 try:
     hours = float(int)
-    if float(hours) > float(40):
-        ot_hours = float(hours) - float(40)
-        rate = 15
-        ot_pay = float(ot_hours) * float(rate)
-        normal_hours = float(40) * float(10)
-        ot_gross = float(ot_pay) + float(normal_hours)
+    rate = 10
+    full_hours = float(40)
+    if hours > full_hours: #short circuited if not true doesn't continue and moves to except:
+        ot_hours = float(hours) - full_hours
+        ot_rate = 15
+        ot_pay = float(ot_hours) * float(ot_rate)
+        ot_gross = float(ot_pay) + computepay(full_hours, rate)
         print(f"${ot_gross} is your gross pay.")
     else:  #nested conditional
-        if float(hours) <= float(40):
-            rate =10
-        pay = float(hours) * float(rate)
+        if hours <= full_hours:
+            pay = computepay(int, rate)
         print(f"${pay} is your gross pay.")
 except:
     print("Error, please enter numeric input")
