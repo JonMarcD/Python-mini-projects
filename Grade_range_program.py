ece473e41755b52d424364edf2a812bfa09c5922
Jon Marc <jonmarcguz@gmail.com>
2022-10-18 17:38:21 -0700

    Grade_range_program build 2:
    There are now three functions one which takes the input and handles legal and illegal values using exceptions and chained conditionals.
    The other function takes the first function and returns a grade as a string associated with the value from the first function.
    The last function just handles printing the grade.

--- Grade_range_program.py
+++ Grade_range_program.py
@@ -1,27 +1,34 @@
-#Takes score input and outputs grade
- 
-inp = input('Enter given score between 0.0-1.0:\n')
+#Takes score input and makes sure the input is a legal value
+def scoreinput():
+    inp = input('Enter given score between 0.0-1.0:\n')
+    try:#exception so that only numbers can be the input
+        score = float(inp)
+        if score < 0.0:#chained conditional that excludes numbers which are not in the range
+            print('Enter given score between 0.0-1.0')
+        elif score > 1.0:
+            print('Please enter score which is in range.')
+    except:
+        print('Please enter number.')
+    return inp #if this function is called it returns inp
 
-#exception so that only numbers can be the input
-try:
-    final_grade = float(inp)
-except:
-    print('Please enter number.')
+#Function that takes the score as a parameter and returns grade as string
+def computegrade(score):
+#A simple chained conditional
+    if score >= 0.9 and score <= 1.0:
+        grade = "A"
+    elif score >= 0.8 and score <=0.85:
+        grade = "B"
+    elif score >= 0.7 and score <= 0.75:
+        grade = "C"
+    elif score >= 0.6 and score <= 0.65:
+        grade = "D"
+    elif score < 0.6 and score >= 0.0:
+        grade = "F"
+    return grade #this just returns the variable grade so if you call for the function the return is grade
 
-#chained conditional that excludes numbers which are not in the range
-if final_grade < 0.0:
-    print('Enter given score between 0.0-1.0')
-elif final_grade > 1.0:
-    print('Please enter score which is in range.')
+#the main part of the program which defines the function scoreinput(): as score and prints out the grade associated with the score
+def main():
+    score = float(scoreinput())
+    print(f"Final grade is {computegrade(score)}")
 
-#chained conditional which has the number range and their respective grade
-if final_grade >= 0.9 and final_grade <= 1.0:
-    print('grade = A')
-elif final_grade >= 0.8 and final_grade <=0.85:
-    print('grade = B')
-elif final_grade >= 0.7 and final_grade <= 0.75:
-    print('grade = C')
-elif final_grade >= 0.6 and final_grade <= 0.65:
-    print('grade = D')
-elif final_grade < 0.6 and final_grade >= 0.0:
-    print('grade = F')
+main()
