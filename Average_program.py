30a4b8ab8227e41e4bf669917307da9b14b48141
Jon Marc <jonmarcguz@gmail.com>
2022-10-23 19:51:03 -0700

    Average_program build 2:
    The program now functions properly it takes user input and continually runs a loop which keeps asking for user input.
    There are also exceptions to all strings other than "done" which ends the program when it is entered.

--- Average_program.py
+++ Average_program.py
@@ -1,19 +1,26 @@
-#Program takes numbers input by the user and prints out count, total, average
-print("Enter number")
+ #Program takes numbers input by the user and prints out count, total, average
+from operator import truediv
 
-#def data_read(inp):
-while True:
-    data = input()
-    if data[0] == '#':
-        continue
-    if data == "done":
-        break
-    print(data)
-print("Done")
+#this takes the input and checks if its an legal variable which can be used
+def input_source():
+    inp = input("Enter number then write done when finished. ")
+    try:
+        data = inp
+        if data == "done": #this just checks if data is equal to done which is the only acceptable string
+            return data
+        data = int(data) #this makes almost all acceptable values only integers
+    except:
+        if data != int: #if data isn't an integer it runs the if statement
+            print("Bad data. Please write number")
+    return data #this is whats returned from the function
 
-#this just checks if input is legal variable
-#def input_check(inp):
-try:
-    data = int(data)
-except:
-    print("Bad data. Please write number")
+def main(): #where the program runs
+    while True: #a loop which checks if the following statements are true
+        data = input_source() #defines the variable data as input source
+        if data == int: #this checks the data to be an integer and continues the loop
+            continue
+        if data == "done": #if data is the string "done" it stops the loop stoping the program
+            break
+    print("Computation done.")
+
+main()
