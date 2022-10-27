0391d4a06ecdd2f24e37c822d9b9ad7cd03d817a
Jon Marc <jonmarcguz@gmail.com>
2022-10-26 23:00:26 -0700

    Average_program build 3:
    Removed several lines of code an 2 functions as the program could not work because the loop would end due to unknown reason
    will continue to revise this and improve the program. Need to understand the flow and connections of functions better.

--- Average_program.py
+++ Average_program.py
@@ -1,26 +1,21 @@
  #Program takes numbers input by the user and prints out count, total, average
-from operator import truediv
-
-#this takes the input and checks if its an legal variable which can be used
-def input_source():
-    inp = input("Enter number then write done when finished. ")
-    try:
-        data = inp
-        if data == "done": #this just checks if data is equal to done which is the only acceptable string
-            return data
-        data = int(data) #this makes almost all acceptable values only integers
-    except:
-        if data != int: #if data isn't an integer it runs the if statement
-            print("Bad data. Please write number")
-    return data #this is whats returned from the function
 
 def main(): #where the program runs
+    count = 0
+    total = 0
     while True: #a loop which checks if the following statements are true
-        data = input_source() #defines the variable data as input source
-        if data == int: #this checks the data to be an integer and continues the loop
-            continue
-        if data == "done": #if data is the string "done" it stops the loop stoping the program
+        inp = input("Enter number then write done when finished. ")
+        data = inp
+        if data == "done": #this just checks if data is equal to done which is the only acceptable string
             break
-    print("Computation done.")
+        try:
+            data = int(inp)
+        except:
+            if data != int: #if data isn't an integer it runs the if statement
+                print("Bad data. Please write number")
+                continue
+        count = count + 1
+        total = total + data
+    print(f"Computation done. Count: {count}. Total: {total}. Average: {total/count}")
 
 main()
