cd5fcc1df56862b3e25c57220c50b7195eb9b58f
Jon Marc <jonmarcguz@gmail.com>
2022-11-02 16:22:23 -0700

    Backwards_string_program build 2:
    The program now handles the exceptions of integers through a error handling function that checks if there are integers
    in the input then raises a ValueError which prints an error message. This will also return a different value that
    is checked for in the main function and breaks the loop if the value returned matches the if statement.
    
    The program continues as normal when entering a legal variable. Still is the program doesn't print letters on the
    same line in the terminal.

--- Backwards_string_program.py
+++ Backwards_string_program.py
@@ -2,9 +2,25 @@
 
 prompt = print("Enter a word with 8 characters.")
 inp = input()
-index = -1 #this sets the index as -1 so that it when the input is read it starts at the last letter
 
-while index < len(inp): #this statement is always true until the entire word is printed then the index will = character the statement will then become false
-    letter = inp[index] #this says the index of the input and gives letter a definition
-    print(letter), #prints the letter
-    index = index - 1 #this just reads the word backwards allowing it to  be printed backwards
+def error_handling():
+    data = inp
+    try:
+        if any(i.isdigit() for i in data):
+            raise ValueError('No integers')
+    except ValueError as e:
+        print(e)
+        return print
+    return data
+
+def main():
+    inp = error_handling()
+    index = -1 #this sets the index as -1 so that it when the input is read it starts at the last letter
+    while index < len(inp): #this statement is always true until the entire word is printed then the index will = character the statement will then become false
+        letter = inp[index] #this says the index of the input and gives letter a definition
+        if error_handling() == (print):
+            break
+        print(letter), #prints the letter
+        index = index -1 #this just reads the word backwards allowing it to  be printed 
+main()
+
