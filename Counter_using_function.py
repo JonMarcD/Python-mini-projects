c294683b41e79e654050006d5278fa8b187453d8
Jon Marc <jonmarcguz@gmail.com>
2022-11-02 17:07:13 -0700

    Counter_using_function:
    Just added a comment.

--- Counter_using_function.py
+++ Counter_using_function.py
@@ -2,3 +2,5 @@ word = "banana"
 
 index = word.count('a')
 print(index)
+
+#we can use the function count in this way count(sub/substring[start:end]) this shows us that we can use a split index with it.
